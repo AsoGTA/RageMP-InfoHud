74d7c005c3aacd20a161cdf8cb05ab8aafd8f41b
AsoGTA <108180573+AsoGTA@users.noreply.github.com>
2023-02-01 21:15:43 +0300

    Add files via upload

---
+++ TestScript/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using GTANetworkAPI;
+
+namespace ExampleResource
+{
+
+    public class Main : Script
+    {
+
+        [ServerEvent(Event.PlayerSpawn)]
+        public void OnPlayerSpawn(Player ply) {
+            NAPI.ClientEvent.TriggerClientEvent(ply, "client:showcefInfo", true);
+            NAPI.ClientEvent.TriggerClientEvent(ply, "client:createInfo", "fa-solid fa-star-of-life", "Paramedic", "255, 255, 255", "120,120,120,0.3", "https://www.shutterstock.com/image-illustration/medical-health-red-cross-neon-260nw-1918247282.jpg");
+            NAPI.ClientEvent.TriggerClientEvent(ply, "client:createInfo", "fa-solid fa-user", ply.Name, "255,255,255", "120,120,120,0.6", "https://img.freepik.com/free-vector/minimal-geometric-stripe-shape-background_1409-1014.jpg");
+            NAPI.ClientEvent.TriggerClientEvent(ply, "client:createInfo", "fa-solid fa-money-bill-wave", "1234,50", "0,120,0", "120,120,120,0.9", "https://img.freepik.com/free-vector/green-curve-abstract-background_53876-99569.jpg?w=360");
+        }
+
+    }
+}
---
+++ client_packages/cs_packages/InfoHud.cs
@@ -0,0 +1,76 @@
+﻿using RAGE;
+using System;
+namespace InfoHud
+{
+
+    using HtmlWindow = RAGE.Ui.HtmlWindow;
+
+    public class Main : Events.Script
+    {
+
+        private String[] objToString(object[] obj)
+        {
+            String[] vals = new String[obj.Length];
+            for (int i = 0; i < obj.Length; i++)
+                vals[i] = (String)obj[i];
+
+            return vals;
+
+        }
+
+        HtmlWindow InfoCEF = null;
+
+        public Main()
+        {
+            InfoCEF = new HtmlWindow("package://InfoHud/InfoHud.html");
+            Events.Add("client:showcefInfo", ShowCef);
+            Events.Add("client:createInfo", CreateInfo);
+            Events.Add("client:setInfo", SetInfoValue);
+            Events.Add("client:delInfo", DeleteInfo);
+            Events.Add("client:reloadInfo", RefreshInfo);
+        }
+
+
+        public void ShowCef(object[] args)
+        {
+            InfoCEF.Active = (bool) args[0];
+        }
+
+        public void CreateInfo(object[] args)
+        {
+            String[] vals = objToString(args);
+
+            String execute = "createInfo(";
+            foreach (String val in vals)
+                execute += "'" + val + "', ";
+             execute += ");";
+
+            InfoCEF.ExecuteJs(execute);
+        }
+
+        public void SetInfoValue(object[] args)
+        {
+            String[] vals = objToString(args);
+
+            InfoCEF.ExecuteJs("setInfo(" + vals[0] +", " + vals[1] + ")");
+            
+        }
+
+        public void DeleteInfo(object[] args)
+        {
+
+            String[] vals = objToString(args);
+
+            InfoCEF.ExecuteJs("deleteInfo(" + vals[0] + ");");
+
+        }
+
+        public void RefreshInfo(object[] args)
+        {
+            InfoCEF.Reload(false);
+        }
+
+
+
+    }
+}
